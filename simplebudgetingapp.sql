8d10ba0dbde19b7e6364f2cba23d65de83d56f25
kacansky <57094731+kacansky@users.noreply.github.com>
2021-08-03 14:39:18 +0200

    Add files via upload

---
+++ simplebudgetingapp.sql
@@ -0,0 +1,106 @@
+/*
+SQLyog Community v13.1.6 (64 bit)
+MySQL - 10.4.19-MariaDB : Database - simplebudgetingapp
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`simplebudgetingapp` /*!40100 DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci */;
+
+USE `simplebudgetingapp`;
+
+/*Table structure for table `category` */
+
+DROP TABLE IF EXISTS `category`;
+
+CREATE TABLE `category` (
+  `categoryid` bigint(20) NOT NULL,
+  `name` varchar(100) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `userid` bigint(20) NOT NULL,
+  PRIMARY KEY (`categoryid`,`userid`),
+  KEY `userid` (`userid`),
+  CONSTRAINT `category_ibfk_1` FOREIGN KEY (`userid`) REFERENCES `user` (`userid`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+/*Data for the table `category` */
+
+/*Table structure for table `currency` */
+
+DROP TABLE IF EXISTS `currency`;
+
+CREATE TABLE `currency` (
+  `currencyid` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(30) COLLATE utf8_unicode_ci NOT NULL,
+  `symbol` char(1) COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`currencyid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+/*Data for the table `currency` */
+
+/*Table structure for table `transaction` */
+
+DROP TABLE IF EXISTS `transaction`;
+
+CREATE TABLE `transaction` (
+  `transactionid` bigint(20) NOT NULL,
+  `amount` double DEFAULT NULL,
+  `transaction_type` varchar(10) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `walletid` bigint(20) NOT NULL,
+  `categoryid` bigint(20) NOT NULL,
+  `date` date DEFAULT NULL,
+  PRIMARY KEY (`transactionid`,`walletid`,`categoryid`),
+  KEY `categoryid` (`categoryid`),
+  KEY `walletid` (`walletid`),
+  CONSTRAINT `transaction_ibfk_1` FOREIGN KEY (`categoryid`) REFERENCES `category` (`categoryid`) ON UPDATE CASCADE,
+  CONSTRAINT `transaction_ibfk_2` FOREIGN KEY (`walletid`) REFERENCES `wallet` (`walletid`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+/*Data for the table `transaction` */
+
+/*Table structure for table `user` */
+
+DROP TABLE IF EXISTS `user`;
+
+CREATE TABLE `user` (
+  `userid` bigint(20) NOT NULL,
+  `first_name` varchar(100) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `last_name` varchar(100) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `username` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `password` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`userid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+/*Data for the table `user` */
+
+insert  into `user`(`userid`,`first_name`,`last_name`,`username`,`password`) values 
+(27,'a','a','a','a'),
+(59,'Dunja','Kacanski','duduche','guguche');
+
+/*Table structure for table `wallet` */
+
+DROP TABLE IF EXISTS `wallet`;
+
+CREATE TABLE `wallet` (
+  `walletid` bigint(20) NOT NULL,
+  `name` varchar(100) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `balance` double DEFAULT NULL,
+  `currencyid` bigint(20) NOT NULL,
+  `userid` bigint(20) NOT NULL,
+  PRIMARY KEY (`walletid`,`userid`,`currencyid`),
+  KEY `user_change` (`userid`),
+  CONSTRAINT `user_change` FOREIGN KEY (`userid`) REFERENCES `user` (`userid`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+/*Data for the table `wallet` */
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
